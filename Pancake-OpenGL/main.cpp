39c00fca15b95a15f96d2a1ab3ea37e066178ec4
kidagine <davidkalatzis1999@hotmail.com>
2021-03-03 00:58:14 +0100

    Shader update and refactoring.

--- Pancake-OpenGL/main.cpp
+++ Pancake-OpenGL/main.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "Shader.h" 
 
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
-const GLchar *vertexShaderSource = "#version 330 core\n"
-"layout ( location = 0 ) in vec3 position;\n" 
-"void main ( )\n"
-"{\n"
-"gl_Position = vec4( position.x, position.y, position.z, 1.0 );\n"
-"}";
-
-const GLchar* fragmentShaderSource = "#version 330 core\n"
-"out vec4 color;\n"
-"void main()\n"
-"{\n"
-"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
 
 
 int main()
@@ -27,8 +15,8 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Pancake-OpenGL", NULL, NULL);
 
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Pancake-OpenGL", NULL, NULL);
 	if (window != NULL)
 	{
 		glfwMakeContextCurrent(window);
@@ -37,77 +25,42 @@ int main()
 		if (GLEW_OK == glewInit())
 		{
 			glViewport(0, 0, WIDTH, HEIGHT);
-			GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-			glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-			glCompileShader(vertexShader);
-			GLint success;
-			GLchar infoLog[512];
+			Shader shader("Resources/Shaders/core.vs", "Resources/Shaders/core.frag");
+			GLfloat vertices[] =
+			{
+				-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f,
+				0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
+				0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f
+			};
 			GLuint VBO, VAO;
-			GLuint shaderProgram;
-			glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-			if (success)
+			glGenVertexArrays(1, &VAO);
+			glGenBuffers(1, &VBO);
+			glBindVertexArray(VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) 0);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+			glEnableVertexAttribArray(1);
+
+			glBindVertexArray(0);
+			while (!glfwWindowShouldClose(window))
 			{
-				glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-				GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-				glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-				glCompileShader(fragmentShader);
-				glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-				if (success)
-				{
-					shaderProgram = glCreateProgram();
-					glAttachShader(shaderProgram, vertexShader);
-					glAttachShader(shaderProgram, fragmentShader);
-					glLinkProgram(shaderProgram);
-					glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-					if (success)
-					{
-						glDeleteShader(vertexShader);
-						glDeleteShader(fragmentShader);
-						GLfloat vertices[] =
-						{
-							-0.5f, -0.5f, 0.0f,
-							0.5f, -0.5f, 0.0f,
-							0.0f, 0.5f, 0.0f
-						};
-						glGenVertexArrays(1, &VAO);
-						glGenBuffers(1, &VBO);
-						glBindVertexArray(VAO);
-						glBindBuffer(GL_ARRAY_BUFFER, VBO);
-						glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-						glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-						glEnableVertexAttribArray(0);
-						glBindBuffer(GL_ARRAY_BUFFER, 0);
-						glBindVertexArray(0);
-						while (!glfwWindowShouldClose(window))
-						{
-							glfwPollEvents();
+				glfwPollEvents();
 
-							glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-							glClear(GL_COLOR_BUFFER_BIT);
+				glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+				glClear(GL_COLOR_BUFFER_BIT);
 
-							glUseProgram(shaderProgram);
-							glBindVertexArray(VAO);
-							glDrawArrays(GL_TRIANGLES, 0, 3);
-							glBindVertexArray(0);
+				shader.Use();
+				glBindVertexArray(VAO);
+				glDrawArrays(GL_TRIANGLES, 0, 3);
+				glBindVertexArray(0);
 
-							glfwSwapBuffers(window);
-						}
-					}
-					else
-					{
-						printf("Shader program error");
-					}
-				}
-				else
-				{
-					printf("Shader fragment error");
-				}
+				glfwSwapBuffers(window);
 			}
-			else
-			{
-				printf("Shader vertex  error");
-			}
-				glDeleteVertexArrays(1, &VAO);
+
+			glDeleteVertexArrays(1, &VAO);
 			glDeleteBuffers(1, &VBO);
 			glfwTerminate();
 		}
