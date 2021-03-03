402025947f08ace3b0cb6cf717f0c7595dde55b6
kidagine <davidkalatzis1999@hotmail.com>
2021-03-03 13:29:31 +0100

    Adding transformation, SDL2 and minor refactoring

--- Pancake-OpenGL/main.cpp
+++ Pancake-OpenGL/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <SDL_image.h>
 #include "Shader.h" 
+
 
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
@@ -25,26 +32,56 @@ int main()
 		if (GLEW_OK == glewInit())
 		{
 			glViewport(0, 0, WIDTH, HEIGHT);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 			Shader shader("Resources/Shaders/core.vs", "Resources/Shaders/core.frag");
 			GLfloat vertices[] =
 			{
-				-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f,
-				0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
-				0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f
+				///Position			//Color				//Texture	
+				0.5f, 0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	1.0f, 1.0f,
+				0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	1.0f, 0.0f,
+				-0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f,	0.0f, 0.0f,
+				-0.5f, 0.5f, 0.0f,	1.0f, 1.0f, 0.0f,	0.0f, 1.0f
 			};
-			GLuint VBO, VAO;
+			GLuint indices[] =
+			{
+				0, 1, 3,
+				1, 2, 3
+			};
+
+			GLuint VBO, VAO, EBO;
 			glGenVertexArrays(1, &VAO);
 			glGenBuffers(1, &VBO);
+			glGenBuffers(1, &EBO);
 			glBindVertexArray(VAO);
+
 			glBindBuffer(GL_ARRAY_BUFFER, VBO);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) 0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*) 0);
 			glEnableVertexAttribArray(0);
-			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+			glEnableVertexAttribArray(2);
+				glBindVertexArray(0);
 
-			glBindVertexArray(0);
+			GLuint texture;
+			int width = 0;
+			int height = 0;
+			glGenTextures(1, &texture);
+			glBindTexture(GL_TEXTURE_2D, texture);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			SDL_Surface* image = IMG_Load("Resources/Images/pancakes.jpg");
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->w, image->h, 0, GL_RGB, GL_UNSIGNED_BYTE, image->pixels);
+			glGenerateMipmap(GL_TEXTURE_2D);
+			SDL_FreeSurface(image);
+			glBindTexture(GL_TEXTURE_2D, 0);
 			while (!glfwWindowShouldClose(window))
 			{
 				glfwPollEvents();
@@ -53,8 +90,20 @@ int main()
 				glClear(GL_COLOR_BUFFER_BIT);
 
 				shader.Use();
+
+				glm::mat4 transform(1);
+				transform = glm::translate(transform, glm::vec3(0));
+				transform = glm::rotate(transform, (GLfloat)glfwGetTime() * -1.0f, glm::vec3(1.0f, 1.0f, 0.0f));
+
+				GLint transformLocation = glGetUniformLocation(shader.Program, "transform");
+				glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transform));
+
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_2D, texture);
+				glUniform1i(glGetUniformLocation(shader.Program, "ourTexture"), 0);
+
 				glBindVertexArray(VAO);
-				glDrawArrays(GL_TRIANGLES, 0, 3);
+				glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 				glBindVertexArray(0);
 
 				glfwSwapBuffers(window);
@@ -62,6 +111,7 @@ int main()
 
 			glDeleteVertexArrays(1, &VAO);
 			glDeleteBuffers(1, &VBO);
+			glDeleteBuffers(1, &EBO);
 			glfwTerminate();
 		}
 	}
