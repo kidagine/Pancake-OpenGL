eebf508064ff2a40075c0c2e694287484efdc435
kidagine <davidkalatzis1999@hotmail.com>
2021-03-03 14:50:21 +0100

    Added camera.

--- Pancake-OpenGL/main.cpp
+++ Pancake-OpenGL/main.cpp
@@ -7,12 +7,26 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <SDL_image.h>
+#include "Camera.h"
 #include "Shader.h" 
 
 
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
 
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+void MouseCallback(GLFWwindow* window, double xPos, double yPos);
+void DoMovement();
+
+Camera  camera(glm::vec3(0.0f, 0.0f, 3.0f));
+GLfloat lastX = WIDTH / 2.0;
+GLfloat lastY = HEIGHT / 2.0;
+bool keys[1024];
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
 
 int main()
 {
@@ -29,6 +43,11 @@ int main()
 		glfwMakeContextCurrent(window);
 		glewExperimental = GL_TRUE;
 
+        glfwSetKeyCallback(window, KeyCallback);
+        glfwSetCursorPosCallback(window, MouseCallback);
+        glfwSetScrollCallback(window, ScrollCallback);
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 		if (GLEW_OK == glewInit())
 		{
 			glViewport(0, 0, WIDTH, HEIGHT);
@@ -38,51 +57,6 @@ int main()
 
 			Shader shader("Resources/Shaders/core.vs", "Resources/Shaders/core.frag");
 
-            /*
-            GLfloat vertices[] = {
-                -0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,
-                0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 0.0f,
-                0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-                0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-                -0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-                -0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,
-
-                -0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-                0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-                0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,
-                0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,
-                -0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 1.0f,
-                -0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-
-                -0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-                -0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-                -0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-                -0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-                -0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-                -0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-
-                0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-                0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-                0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-                0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-                0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-                0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-
-                -0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-                0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-                0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-                0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-                -0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-                -0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-
-                -0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-                0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-                0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-                0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-                -0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-                -0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f
-            };
-            */
             GLfloat vertices[] = {
                 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
                 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -127,6 +101,20 @@ int main()
                 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
             };
 
+            glm::vec3 cubePositions[] =
+            {
+                glm::vec3(0.0f, 0.0f, 0.0f),
+                glm::vec3(2.0f, 5.0f, -15.0f),
+                glm::vec3(-1.5f, -2.2f, -2.5f),
+                glm::vec3(-3.8f, -2.0f, -12.3f),
+                glm::vec3(2.4f, -0.4f, -3.5f),
+                glm::vec3(-1.7f, 3.0f, -7.5f),
+                glm::vec3(1.3f, -2.0f, -2.5f),
+                glm::vec3(1.5f, 2.0f, -2.5f),
+                glm::vec3(1.5f, 0.2f, -1.5f),
+                glm::vec3(-1.3f, 1.0f, -1.5f)
+            };
+
 			GLuint VBO, VAO;
 			glGenVertexArrays(1, &VAO);
 			glGenBuffers(1, &VBO);
@@ -161,7 +149,12 @@ int main()
 
 			while (!glfwWindowShouldClose(window))
 			{
+                GLfloat currentFrame = glfwGetTime();
+                deltaTime = currentFrame - lastFrame;
+                lastFrame = currentFrame;
+
 				glfwPollEvents();
+                DoMovement();
 
 				glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -172,21 +165,33 @@ int main()
 
 				shader.Use();
 
+                glm::mat4 projection;
+                projection = glm::perspective(camera.GetZoom(), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 1000.0f);
+
                 glm::mat4 model(1);
                 glm::mat4 view(1);
-                model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 0.1f, 0.0f));
-                view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+                view = camera.GetViewMatrix();
 
                 GLint modelLocation = glGetUniformLocation(shader.Program, "model");
                 GLint viewLocation = glGetUniformLocation(shader.Program, "view");
                 GLint projectionLocation = glGetUniformLocation(shader.Program, "projection");
 
-                glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
                 glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
                 glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
 
                 glBindVertexArray(VAO);
-                glDrawArrays(GL_TRIANGLES, 0, 36);
+
+                for (GLuint i = 0; i < 10; i++)
+                {
+                    glm::mat4 model(1);
+                    model = glm::translate(model, cubePositions[i]);
+                    GLfloat angle = 20.0f * i;
+                    model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+                    glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+                    glDrawArrays(GL_TRIANGLES, 0, 36);
+                }
+
                 glBindVertexArray(0);
 
 				glfwSwapBuffers(window);
@@ -199,4 +204,71 @@ int main()
 	}
 
 	return EXIT_SUCCESS;
+}
+
+void DoMovement()
+{
+    if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
+    {
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+
+    if (keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN])
+    {
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+
+    if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT])
+    {
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    }
+
+    if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
+    {
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+}
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+        {
+            keys[key] = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            keys[key] = false;
+        }
+    }
+}
+
+void MouseCallback(GLFWwindow* window, double xPos, double yPos)
+{
+    if (firstMouse)
+    {
+        lastX = xPos;
+        lastY = yPos;
+        firstMouse = false;
+    }
+
+    GLfloat xOffset = xPos - lastX;
+    GLfloat yOffset = lastY - yPos;
+
+    lastX = xPos;
+    lastY = yPos;
+
+    camera.ProcessMouseMovement(xOffset, yOffset);
+}
+
+
+void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset)
+{
+    camera.ProcessMouseScroll(yOffset);
 }
